e35c22755276ad91ad720da3f5ff27c585024c6d
skali3n <darththror@gmail.com>
2023-11-08 19:18:04 +0200

    1

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,10 +4,10 @@ import { validateUser } from '../dto/index.js';
 export const router = Router();
 
 export class UsersController {
-  #usersService;
+  _usersService;
 
   constructor(usersService) {
-    this.#usersService = usersService;
+    this._usersService = usersService;
 
     router.post('/', validateUser, this.postUser);
     router.get('/', this.getUsers);
@@ -16,7 +16,7 @@ export class UsersController {
   async postUser(req, res, next) {
     try {
       const input = req.body;
-      const user = await this.#usersService.createUser(input);
+      const user = await this._usersService.createUser(input);
 
       res.status(201).json(user);
     } catch (err) {
@@ -28,7 +28,7 @@ export class UsersController {
 
   async getUsers(_, res, next) {
     try {
-      const users = await this.#usersService.getUsers();
+      const users = await this._usersService.getUsers();
 
       res.status(200).json(users);
     } catch (err) {
